7a0c07150ddc5a7f28dc7fe2f043edf899b5fe50
rajha <rajha@rajhansji>
2023-04-04 13:15:31 +0530

    dfd

--- src/main/java/Syed.java
+++ src/main/java/Syed.java
@@ -2,7 +2,8 @@
 public class Syed {
 public static void main(String[] args) {
 	System.out.println("Syed");
-	System.out.println("Veeresh");
+	System.out.println("Veereshhh");
 	System.out.println("Naseer");
+	System.out.println("Raju");
 }
 }
